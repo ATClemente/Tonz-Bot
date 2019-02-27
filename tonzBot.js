87b875fb62396b3d5756c9616f2b6dd3348adebb
ATClemente <anthony.clemente13@gmail.com>
2019-02-26 21:24:49 -0500

    Set up basic framework
    -Set up basic framework and started trying to implement some features

--- tonzBot.js
+++ tonzBot.js
@@ -1,98 +1,50 @@
-/*var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			case 'Hello!':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hi there kind humans!'
-                });
-			break;
-			case 'Doom':
-                bot.sendMessage({
-                    to: channelID,
-                    message: '<:doom:508093498537410560>'
-                });
-			break;
-			case 'Users':
-			    bot.sendMessage({
-                    to: channelID,
-                    message: bot.guilds.first.members.size()
-                });
-			break;
-            // Just add any case commands if you want to..
-         }
-     }
-});*/
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 var auth = require('./auth.json');
+var commandsList = require('./constants.json').commands;
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-  /*if (message.content === 'ping') {
-    message.reply('Pong!');
-  }*/
-  
+
+/*if(message.tts){
+	message.delete();
+	message.reply('Why the /tts though?');
+}*/
+
+
 if (message.content.substring(0, 1) == '!') {
+	
+	
 	var args = message.content.substring(1).split(' ');
 	var cmd = args[0];
 
 	args = args.splice(1);
 	switch(cmd) {
 		// !ping
-		case 'ping':
+		case commandsList[0].name:
 			message.reply('Pong!');
 		break;
-		case 'Hello!':
+		case commandsList[1].name:
 			message.reply('Hi there kind humans!');
 		break;
-		case 'Doom':
+		case commandsList[2].name:
 			message.channel.send('<:doom:508093498537410560>');
 		break;
-		case 'Delete':
+		case commandsList[3].name:
 			message.delete()
 				.then(message => console.log(`Deleted message from ${message.author.username}`))
 				.catch(console.error);
 		break;
-		case 'Random':
+		case commandsList[4].name:
 			message.channel.send(client.users.random().username);
+			//console.log(client.users.random().username);
+		break;
+		case commandsList[5].name:
+			message.channel.send(commandList());
 			//console.log(client.users.random().username);
 		break;
 		// Just add any case commands if you want to..
@@ -100,4 +52,47 @@ if (message.content.substring(0, 1) == '!') {
 	}
 });
 
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+	//console.log("Voice state changed");
+	//console.log(newMember.user);
+	if(newMember.user.bot){
+		
+		console.log("Bot voice state updated");
+		
+	}
+});
+
+
+client.on('guildMemberSpeaking', (member, speaking) => {
+	console.log("Speaking");
+	//console.log(newMember.user);
+	if(member.user.bot){
+		
+		console.log("Bot is speaking");
+		
+	}
+});
+
+function setBotVolume(){
+	
+	console.log(client.users);
+	
+}
+
+function commandList(){
+	var output = '';
+	for(var c in commandsList){
+		output += commandsList[c].name + ': ' + commandsList[c].description + '\n';
+		
+	}
+	
+	return output;	
+}
+
+
+//console.log(commandsList[0].name);
+//console.log(commandList());
+
 client.login(auth.token);
+setBotVolume();
